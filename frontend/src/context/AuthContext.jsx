5c5462b96e66793e1715806667da7c711baa73f9
Mamy Lainé Soumaoro <lainemamysoumaoro@gmail.com>
2026-04-07 12:29:24 +0000

    UPDATE: add a comment to authcontext to get rid of the error in the terminal

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+
 import { createContext, useContext, useReducer, useEffect, useCallback } from 'react';
 
 const AuthContext = createContext(null);
