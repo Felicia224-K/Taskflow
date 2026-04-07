b68492b9f3c90d0447947a68c24787f1b130663e
Mamy Lainé Soumaoro <lainemamysoumaoro@gmail.com>
2026-04-07 16:11:23 +0000

    DEPLOY: the git repo postgres redis, update database and redis config for railway

--- api/src/config/redis.js
+++ api/src/config/redis.js
@@ -1,6 +1,8 @@
 const Redis = require('ioredis');
 
-const redis = new Redis({
+const redis = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis ({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
 });
